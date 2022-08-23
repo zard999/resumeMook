d931207aafa1fc138a6dd9565e21769aa58bf514
zyh <zhangyh@stpass.com>
2022-08-23 12:07:02 +0800

    feat: 引入babel并配置

---
+++ babel.config.js
@@ -0,0 +1,28 @@
+/*
+ * @Author: zyh
+ * @Date: 2022-08-23 11:59:24
+ * @LastEditors: zyh
+ * @LastEditTime: 2022-08-23 12:01:12
+ * @FilePath: /resume/babel.config.js
+ * @Description: babel配置文件
+ *
+ * Copyright (c) 2022 by 穿越, All Rights Reserved.
+ */
+module.exports = {
+  presets: [
+    "@babel/preset-env", // 👉 根据配置的目标浏览器或者运行环境，选择对应的语法包，从而将代码进行转换
+    "@babel/preset-react", // 👉 react 语法包，让我们可以使用 React ES6 Class Component 的写法，支持JSX、TSX语法格式
+    "@babel/preset-typescript", // 👉 https://github.com/babel/babel/issues/10570
+  ],
+  plugins: [
+    "@babel/plugin-transform-runtime", // 👉 官方提供的插件，作用是减少冗余的代码
+    [
+      "@babel/plugin-transform-modules-commonjs", // 👉 将 ECMAScript modules 转成 CommonJS.
+      {
+        allowTopLevelThis: true,
+        loose: true,
+        lazy: true,
+      },
+    ],
+  ],
+};
