9ac0ded488ecb9ff437b8b1571d29042c8760ba4
zyh <zhangyh@stpass.com>
2022-08-24 17:38:44 +0800

    feat: 简历工具条添加和删除

--- app/renderer/container/Resume/ResumeToolbar/index.tsx
+++ app/renderer/container/Resume/ResumeToolbar/index.tsx
@@ -2,7 +2,7 @@
  * @Author: zyh
  * @Date: 2022-08-24 15:49:55
  * @LastEditors: zyh
- * @LastEditTime: 2022-08-24 16:59:55
+ * @LastEditTime: 2022-08-24 17:33:03
  * @FilePath: /resume/app/renderer/container/Resume/ResumeToolbar/index.tsx
  * @Description: 工具条
  *
@@ -17,6 +17,21 @@ function ResumeToolbar() {
   const [addToolbarList, setAddToolbarList] = useState<TSResume.SliderItem[]>([]);
   const [unAddToolbarList, setUnAddToolbarList] = useState<TSResume.SliderItem[]>([]);
 
+  // 添加模块
+  const unToAdd = (item: TSResume.SliderItem, index: number) => {
+    unAddToolbarList.splice(index, 1);
+    addToolbarList.push(item);
+    setUnAddToolbarList([...unAddToolbarList]);
+    setAddToolbarList([...addToolbarList]);
+  };
+
+  // 删除模块
+  const addToUn = (item: TSResume.SliderItem, index: number) => {
+    addToolbarList.splice(index, 1);
+    unAddToolbarList.push(item);
+    setUnAddToolbarList([...unAddToolbarList]);
+    setAddToolbarList([...addToolbarList]);
+  };
   useEffect(() => {
     if (RESUME_TOOLBAR_LIST.length > 0) {
       let _addToolbarList: TSResume.SliderItem[] = [];
@@ -44,15 +59,22 @@ function ResumeToolbar() {
               已添加模块
             </div>
             <div styleName="content">
-              {addToolbarList.map((toolbar: TSResume.SliderItem) => {
+              {addToolbarList.map((addToolbar: TSResume.SliderItem, index: number) => {
                 return (
-                  <div styleName="box" key={toolbar.key}>
+                  <div styleName="box" key={addToolbar.key}>
                     <div styleName="info">
                       <i styleName="icon" />
                       <div styleName="text">
-                        <div styleName="name">{toolbar.name}</div>
-                        <div styleName="summary">{toolbar.summary}</div>
+                        <div styleName="name">{addToolbar.name}</div>
+                        <div styleName="summary">{addToolbar.summary}</div>
                       </div>
+                      {addToolbar.require && <div styleName="tips">必选项</div>}
+                      {!addToolbar.require && (
+                        <div styleName="action">
+                          <i styleName="edit" onClick={(e: React.MouseEvent) => {}} />
+                          <i styleName="delete" onClick={() => addToUn(addToolbar, index)} />
+                        </div>
+                      )}
                     </div>
                   </div>
                 );
@@ -68,14 +90,14 @@ function ResumeToolbar() {
               未添加模块
             </div>
             <div styleName="content">
-              {unAddToolbarList.map((toolbar: TSResume.SliderItem) => {
+              {unAddToolbarList.map((unAddToolbar: TSResume.SliderItem, index: number) => {
                 return (
-                  <div styleName="box" key={toolbar.key}>
+                  <div styleName="box" key={unAddToolbar.key} onClick={() => unToAdd(unAddToolbar, index)}>
                     <div styleName="info">
                       <i styleName="icon" />
                       <div styleName="text">
-                        <div styleName="name">{toolbar.name}</div>
-                        <div styleName="summary">{toolbar.summary}</div>
+                        <div styleName="name">{unAddToolbar.name}</div>
+                        <div styleName="summary">{unAddToolbar.summary}</div>
                       </div>
                     </div>
                   </div>
