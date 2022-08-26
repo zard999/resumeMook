284f89d031cc8e4a8a757672e1e9bcdc157bd9f7
zyh <zhangyh@stpass.com>
2022-08-26 16:35:41 +0800

    fix: 项目经验静态页面的调整

--- app/renderer/container/Resume/ResumeContent/UseTemplate/templateOne/components/Project/index.tsx
+++ app/renderer/container/Resume/ResumeContent/UseTemplate/templateOne/components/Project/index.tsx
@@ -2,7 +2,7 @@
  * @Author: zyh
  * @Date: 2022-08-24 15:38:23
  * @LastEditors: zyh
- * @LastEditTime: 2022-08-25 13:47:37
+ * @LastEditTime: 2022-08-26 16:34:38
  * @FilePath: /resume/app/renderer/container/Resume/ResumeContent/UseTemplate/templateOne/components/Project/index.tsx
  * @Description: 项目经验
  *
@@ -24,28 +24,41 @@ function Project() {
           projectExperience?.map((experience: TSResume.ProjectExperience, index: number) => (
             <li styleName="flex" key={experience?.content + String(index)}>
               <div styleName="left">
-                <p>
-                  <span>
-                    {experience?.beginTime} - {experience?.endTime}
-                  </span>
-                </p>
+                {(experience?.beginTime || experience?.endTime) && (
+                  <p>
+                    {experience?.beginTime && !experience?.endTime && <span>{experience?.beginTime}</span>}
+                    {!experience?.beginTime && experience?.endTime && <span>{experience?.endTime}</span>}
+                    {experience?.beginTime && experience?.endTime && (
+                      <span>
+                        {experience?.endTime} - {experience?.endTime}
+                      </span>
+                    )}
+                  </p>
+                )}
               </div>
               <div styleName="right">
                 <p>
-                  <span>
-                    {experience?.projectName} -{experience?.post}
-                  </span>
+                  {experience?.projectName && !experience?.post && <span>{experience?.projectName}</span>}
+                  {!experience?.projectName && experience?.post && <span>{experience?.post}</span>}
+                  {experience?.projectName && experience?.post && (
+                    <span>
+                      {experience?.post} - {experience?.post}
+                    </span>
+                  )}
                 </p>
               </div>
               <div styleName="text">
                 <ul styleName="item-box">
-                  {experience?.parseContent &&
-                    experience?.parseContent?.length > 0 &&
-                    experience?.parseContent?.map((value: string, index: number) => (
-                      <li styleName="item-content" key={value + String(index)}>
-                        <span>{value}</span>
-                      </li>
-                    ))}
+                  {experience?.content &&
+                    experience?.parseContent &&
+                    experience?.parseContent.length > 0 &&
+                    experience?.parseContent?.map((content: string, idx: number) => {
+                      return (
+                        <li styleName="item-content" key={idx}>
+                          <span>{content}</span>
+                        </li>
+                      );
+                    })}
                 </ul>
               </div>
             </li>
