e92d31a67d37a5e96b38b3d074239e17fb5965ec
zyh <zhangyh@stpass.com>
2022-08-31 11:42:25 +0800

    feat: 使用url-loader优化图片打包

--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -2,7 +2,7 @@
  * @Author: zyh
  * @Date: 2022-08-23 13:41:34
  * @LastEditors: zyh
- * @LastEditTime: 2022-08-24 17:08:10
+ * @LastEditTime: 2022-08-31 11:41:21
  * @FilePath: /resume/webpack/webpack.base.js
  * @Description: webpack配置文件
  *
@@ -45,11 +45,20 @@ module.exports = {
         test: /\.(jpg|png|jpeg|gif)$/,
         use: [
           {
-            loader: 'file-loader',
+            // loader: 'file-loader',
+            loader: 'url-loader',
             options: {
+              /**
+               * 当图片小于2kb时，图片会自动转成base64格式，
+               * 以字符串形式一起打包到js文件中，从而减少图片请求的数量，
+               * 而大于2kb的图片，则是file-loader处理
+               * url-loader是基于file-loader之上的
+               * 缺点：1. 增大了打包后的js体积。2. 这些图片无法做到按需加载
+               */
+              limit: 2048,
               name: '[name]_[hash].[ext]',
               outputPath: 'images/',
-              esModule: false,
+              // esModule: false,
             },
           },
         ],
